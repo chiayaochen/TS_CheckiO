da0477f0ac253c78e603bc8815c19fe4944826b2
ChiaYao Chen <chiayao.chen@gmail.com>
2020-12-09 19:16:56 +0800

    Practice on 20201209

---
+++ HOME/Sum_Numbers.ts
@@ -0,0 +1,23 @@
+// 引入 assert 函式
+const assert = require('assert');
+
+// My answer
+function sumNumbers(test) {
+    // your code here
+    return 0;
+}
+
+console.log('Example:');
+console.log(sumNumbers('hi'));
+
+// These "asserts" are used for self-checking
+assert.equal(sumNumbers('hi'), 0);
+assert.equal(sumNumbers('who is 1st here'), 0);
+assert.equal(sumNumbers('my numbers is 2'), 2);
+assert.equal(sumNumbers('This picture is an oil on canvas '
+ + 'painting by Danish artist Anna '
+ + 'Petersen between 1845 and 1910 year'), 3755);
+assert.equal(sumNumbers('5 plus 6 is'), 11);
+assert.equal(sumNumbers(''), 0);
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
