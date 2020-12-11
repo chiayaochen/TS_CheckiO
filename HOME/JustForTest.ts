05f441e0f3b0895a5ac74492acb41b610315ceba
ChiaYao Chen <chiayao.chen@gmail.com>
2020-12-11 17:04:36 +0800

    Practice 20201211_1

---
+++ HOME/JustForTest.ts
@@ -0,0 +1,4 @@
+var str = '12 23 56a b78 cde f5g 34 45';
+var arr = str.split(' ');
+var after_map = arr.map(Number);
+console.log(after_map);
--- HOME/Sum_Numbers_ClearAns_1.ts
+++ HOME/Sum_Numbers_ClearAns_1.ts
@@ -1,24 +1,13 @@
 // 引入 assert 函式
 const assert = require('assert');
 
-// My answer
-function sumNumbers(test) {
-    // your code here
-    // 先篩選出文字
-    var total = 0
-    var text = test.match(/\w+/g)
-    for (var i in text) {
-        // 篩選出有數字的
-        var num = Number(text[i]);
-        if (num){
-            total += num;
-        }
-    }
-    return total;
-}
-
-console.log('Example:');
-console.log(sumNumbers('hi'));
+// 手工做一個相加的函式
+var sum = (a, b) => a + b;
+var sumNumbers = t => t
+    .split(' ')        // 將字串按照空白去做切割，得到陣列
+    .map(Number)       // 用 map 把前一個陣列的元素都通過 Number() 轉換，文字則會變成 NaN
+    .filter(isFinite)  // 用 filter 通過 isFinite() 把有限數字過濾出來
+    .reduce(sum, 0);   // 第二個參數代表累積器的初始值為 0，第一個參數則是每次要累加計算的函式
 
 // These "asserts" are used for self-checking
 assert.equal(sumNumbers('hi'), 0);
