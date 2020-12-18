a874e66d2e0a4d60a6d90a2647d842a89379374a
ChiaYao Chen <chiayao.chen@gmail.com>
2020-12-18 18:30:12 +0800

    Practice on  20201218_2

---
+++ HOME/Bigger_Price.ts
@@ -0,0 +1,35 @@
+// 引入 assert 函式
+const assert = require('assert');
+
+interface Stock {
+    name: string,
+    price: number,
+};
+
+function biggerPrice(limit: number, data: Stock[]): Stock[] {
+    // your code here
+    return [];
+}
+
+console.log('Example:')
+console.log(biggerPrice(2, [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]))
+
+assert.deepEqual(biggerPrice(2, [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]), [
+    {"name": "wine", "price": 138},
+    {"name": "bread", "price": 100}
+])
+assert.deepEqual(biggerPrice(1, [
+    {"name": "pen", "price": 5},
+    {"name": "whiteboard", "price": 170}
+]), [{"name": "whiteboard", "price": 170}])
+console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
